3fb92a60168319f07d02e7972c146988e37b5e92
Naveen Nata Raja <naveennraja@gmail.com>
2021-03-23 17:22:26 -0400

    Brushing UI Changes

--- src/App.js
+++ src/App.js
@@ -5,38 +5,38 @@ import Homepage from './components/pages/Homepage';
 function App() {
   return (
     <Router>
-      <main className='container'>
-        <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
-          <button
-            className='navbar-toggler collapsed'
-            type='button'
-            data-toggle='collapse'
-            data-target='#navbarColor01'
-            aria-controls='navbarColor01'
-            aria-expanded='false'
-            aria-label='Toggle navigation'>
-            <span className='navbar-toggler-icon'></span>
-          </button>
-          <div className='collapse navbar-collapse' id='navbarColor01'>
-            <ul className='navbar-nav mr-auto'>
-              <li className='nav-item'>
-                <Link className='nav-link' to='/prowork-task'>
-                  Prowork
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link className='nav-link' to='/addUsers'>
-                  Add User
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link className='nav-link' to='/showUsers'>
-                  Show User
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
+      <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
+        <button
+          className='navbar-toggler collapsed'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarColor01'
+          aria-controls='navbarColor01'
+          aria-expanded='false'
+          aria-label='Toggle navigation'>
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='collapse navbar-collapse' id='navbarColor01'>
+          <ul className='navbar-nav mr-auto'>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/prowork-task'>
+                Prowork
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/addUsers'>
+                Add User
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/showUsers'>
+                Show User
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <main className='container-fluid'>
         <Route path='/prowork-task' exact component={Homepage} />
         <Route path='/addUsers' component={Form} />
         <Route path='/showUsers' component={showUser} />
